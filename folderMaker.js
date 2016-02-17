d1d10d0f8e7463508ccf41503fd86e586366db7d
Kiran Gadhave <kiran@ubiquitoustechsolutions.com>
2016-02-18 00:01:09 +0530

    Folder Structure is being read. Saved to a object linewise. Can diff between a file and folder. TODO: 1. write algorithm to detect level of folder. 2. write alogrithm to make the directories and touch files.

---
+++ folderMaker.js
@@ -0,0 +1,51 @@
+var fs              = require('fs'),
+    filename        = process.argv[2],
+    folderStructure = null;
+    newLine         = {};
+
+
+fs.readFile(filename,'utf8', function(err, data){
+    if(err)
+        throw err;
+    else
+    {
+
+        folderStructure = data;
+        lineSplit(folderStructure);
+        folderMake(newLine);
+
+    }
+});
+
+
+lineSplit = function(folderStructure){
+    var letterCount = 0;
+    var lineNo = 0;
+
+    for (var i = 0; i<folderStructure.length; i++){
+        if (folderStructure.charAt(i) === '\n')
+        {
+            var key = "Line";
+            key = key + lineNo.toString();
+            newLine[key] = folderStructure.substring(letterCount, i);
+            lineNo++;
+            //console.log(newLine[key]);
+            letterCount = i;
+            //console.log("###########");
+        }
+    }
+
+
+}
+
+folderMake = function(newLine){
+    for(var prop in newLine){
+        var folderFile = {name:"", ff:""};
+        folderFile.name = newLine[prop];
+            if(folderFile.name.indexOf(".") != -1)
+                folderFile.ff = "file";
+            else
+                folderFile.ff = "folder";
+
+    }
+}
